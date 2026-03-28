dbc44b35ab2079e4f18b5bd823946f9f356f4700
Tailor <526077247@qq.com>
2026-03-28 14:49:22 +0800

    收集资源依赖

--- Assets/Scripts/Editor/YooAssets/FilterRuleExtends.cs
+++ Assets/Scripts/Editor/YooAssets/FilterRuleExtends.cs
@@ -18,14 +18,42 @@ namespace YooAsset
                    data.AssetPath.Contains("Common");
         }
     }
+    
+    [DisplayName("收集Unit依赖")]
+    public class CollectUnitDepend : IFilterRule
+    {
+        public bool IsCollectAsset(FilterRuleData data)
+        {
+            if (data.AssetPath.Contains("/Edit/")) return false;
+            var ext = Path.GetExtension(data.AssetPath);
+            if(Define.ConfigType == 1 && ext == ".json") return false;
+            if(Define.ConfigType == 0 && ext == ".bytes") return false;
+            return !(ext == ".prefab" || ext == ".bytes"|| ext == ".json" || ext == ".controller" || 
+                   data.AssetPath.Contains("Common"));
+        }
+    }
 	
     [DisplayName("收集UI")]
     public class CollectUI : IFilterRule
     {
         public bool IsCollectAsset(FilterRuleData data)
         {
+            if (data.AssetPath.Contains("/Animations/")) return false;
             if (data.AssetPath.Contains("/Atlas/")) return false;
+            if (data.AssetPath.Contains("/Spine/")) return data.AssetPath.EndsWith("asset");
             return true;
+        }
+    }
+    
+    [DisplayName("收集UI依赖")]
+    public class CollectUIDepend : IFilterRule
+    {
+        public bool IsCollectAsset(FilterRuleData data)
+        {
+            if (data.AssetPath.Contains("/Animations/")) return true;
+            if (data.AssetPath.Contains("/Atlas/")) return true;
+            if (data.AssetPath.Contains("/Spine/")) return !data.AssetPath.EndsWith("asset");
+            return false;
         }
     }
         
