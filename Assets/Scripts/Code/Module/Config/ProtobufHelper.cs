aefc3f3691f816e56efcb8d592027b80649db0e4
Tailor <526077247@qq.com>
2026-07-30 14:00:16 +0800

    Excel配置全部懒加载反序列化

--- Assets/Scripts/Code/Module/Config/ProtobufHelper.cs
+++ Assets/Scripts/Code/Module/Config/ProtobufHelper.cs
@@ -1,45 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-#if NOT_UNITY
-using System.ComponentModel;
-#endif
 using ProtoBuf.Meta;
 using System.IO;
-using System.Numerics;
 using ProtoBuf;
 
 namespace TaoTie
 {
-    [ProtoContract]
-    public class BigIntegerSurrogate
-    {
-        [ProtoMember(1, OverwriteList = true)]
-        public byte[] Data { get; set; }
-
-        public static implicit operator BigInteger(BigIntegerSurrogate surrogate)
-            => new BigInteger(surrogate.Data);
-
-        public static implicit operator BigIntegerSurrogate(BigInteger value)
-            => new BigIntegerSurrogate { Data = value.ToByteArray() };
-    }
-
-
     public static class ProtobufHelper
     {
         static ProtobufHelper()
         {
-            // 注册自定义序列化器
-            RuntimeTypeModel.Default.Add(typeof(BigInteger), false).SetSurrogate(typeof(BigIntegerSurrogate));
+            UnityTypeBindings.Init();
         }
-        public static void Init()
-        {
-        }
+
         public static T FromBytes<T>(byte[] bytes)
         {
             if (bytes.Length == 0) return default;
-            var o = (T)FromBytes(typeof(T), bytes, 0, bytes.Length);
+            var o = (T) FromBytes(typeof(T), bytes, 0, bytes.Length);
             return o;
         }
+
         public static object FromBytes(Type type, byte[] bytes, int index, int count)
         {
             using (MemoryStream stream = new MemoryStream(bytes, index, count))
@@ -49,6 +28,7 @@ namespace TaoTie
                 {
                     supportInitialize.EndInit();
                 }
+
                 return o;
             }
         }
@@ -75,6 +55,7 @@ namespace TaoTie
             {
                 supportInitialize.EndInit();
             }
+
             return o;
         }
     }
